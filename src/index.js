2780abccbfd4e71361160e92a51303c23180cfd6
KennetTech <64315533+KennetTech@users.noreply.github.com>
2023-02-04 13:35:53 +0100

    added firebase auth, login createaccount pages.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRRZbWWS-J3-7eV2LeOtv5VIytTbJpgnk",
+  authDomain: "kennet-tech-blog.firebaseapp.com",
+  projectId: "kennet-tech-blog",
+  storageBucket: "kennet-tech-blog.appspot.com",
+  messagingSenderId: "667700500407",
+  appId: "1:667700500407:web:3d977d708dbdf079413a32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
